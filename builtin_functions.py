012bcb4bc462ac5e7b4ae1c6ad74655d716d5d93
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-16 18:34:40 -0400

    Built in functions

---
+++ builtin_functions.py
@@ -0,0 +1,17 @@
+print("Math Functions")
+
+# absolute value function
+abs_int = abs(-1)
+print(abs_int)
+
+# float function will turn any number into a floating point value
+int_to_float = float(100)
+print(int_to_float)
+
+# turns any float into an integer
+print(int(1.23))
+
+print(max(1, 2, -5, 10, 0))
+print(min(1, 2, -5, 10, 0))
+print(pow(2, 3))  # power function
+print(round(51.6))
