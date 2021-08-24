cd83287f06d229da67cb18041f542c86fa0c5d0a
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-24 17:09:12 -0400

    Working with tuples

---
+++ week3/using_tuples.py
@@ -0,0 +1,9 @@
+my_tuple= (1, 2, 3, 4, 5)
+# print(my_tuple[0] + my_tuple[1])
+# print(my_tuple[-1] + my_tuple[-2])
+
+# my_tuple[3] = 1  # cannot do this, tuples are immutable
+# you can do less with tuples but they take up less memory
+
+my_tuple = (5, 4, 3, 2, 1)
+print(my_tuple)
