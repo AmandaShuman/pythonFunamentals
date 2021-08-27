8cf1eb89f632d135083144f2b907d25f73acf81b
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-27 15:51:49 -0400

    updating typo with :: line 17

--- week3/list_slice2.py
+++ week3/list_slice2.py
@@ -1,20 +1,25 @@
-lst = ['Ajay', 'Bobby','Ashok', 'Vijay', 'Anil', 'Rahul','Alex', 'Christopher']
-print (lst[1:])  # prints all but 1st element
+lst = ['Ajay', 'Bobby', 'Ashok', 'Vijay',
+       'Anil', 'Rahul', 'Alex', 'Christopher']
+print(lst[1:])  # prints all but 1st element
 # displays ['Bobby', 'Ashok', 'Vijay', 'Anil', 'Rahul', 'Alex', 'Christopher']
 
-print (lst[0:])  # prints all elements
+print(lst[0:])  # prints all elements
 # displays ['Ajay', 'Bobby', 'Ashok', 'Vijay', 'Anil', 'Rahul', 'Alex', 'Christopher']
 
-print (lst[2:-2]) # all elements starting from third element but skips the last two elements.
+# all elements starting from third element but skips the last two elements.
+print(lst[2:-2])
 # displays ['Ashok', 'Vijay', 'Anil', 'Rahul']
 
-print (lst[::2])  # this will print all alternate elements (begin to end in steps of 2)
+# this will print all alternate elements (begin to end in steps of 2)
+print(lst[::2])
 # displays ['Ajay', 'Ashok', 'Anil', 'Alex']
 
-print (lst[::-1]) # this will print all but last element
+print(lst[::-1])  # prints the list backwards
 # displays ['Christopher', 'Alex', 'Rahul', 'Anil', 'Vijay', 'Ashok', 'Bobby', 'Ajay']
 
-print (lst [-1])  #prints the last element 
-#displays Christopher - note this is not a list, but only a single element
+print(lst[:-1])  # this will print all but last element
 
-print (lst[0:3])
+print(lst[-1])  # prints the last element
+# displays Christopher - note this is not a list, but only a single element
+
+print(lst[0:3])
