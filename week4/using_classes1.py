28841061f8328c80294fc89bd3e1acb73effe9f3
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-09-01 19:39:17 -0400

    creating a class and object

---
+++ week4/using_classes1.py
@@ -0,0 +1,12 @@
+# class names are capitalized
+class Player: 
+    # inside class codeblock are attributes and/or methods
+    max_hp = 4000
+
+# purpose of class is to make objects (instantiate an object)
+
+# how to make objects from classes
+player1 = Player()
+print(player1.max_hp)
+player2 = Player()
+print(player2.max_hp)
