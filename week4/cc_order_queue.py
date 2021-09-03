4d20d66e4202df2c4a37d9ebce6873f1b50bb7b8
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-09-03 11:50:29 -0400

    using queues for ice cream orders

--- week4/cc_order_queue.py
+++ week4/cc_order_queue.py
@@ -29,18 +29,39 @@ class IceCreamShop:
 
         if flavor in self.flavors and 1 <= scoops <= 3:
             print("Order created!")
+            order = {"customer": customer, "flavor": flavor, "scoops": scoops}
+            self.orders.enqueue(order)
         else:
             error_msg_flavor = "We don't have that flavor, please choose another flavor."
             error_msg_scoops = "Please choose between 1 and 3 scoops"
-            print("" if (flavor in self.flavors) else error_msg_flavor)
-            print("" if(1 <= scoops <= 3) else error_msg_scoops)
-
-        order = {"customer": customer, "flavor": flavor, "scoops": scoops}
-        self.orders.enqueue(order)
+            if flavor not in self.flavors:
+                print(error_msg_flavor)
+            else:
+                print(error_msg_scoops)
 
     def show_all_orders(self):
-        print("All Pending Ice Cream Orders")
-        for order in range :  # finish
+        print("\nAll Pending Ice Cream Orders")
+        if self.orders.items is None:  # use none, not zero!
+            print("No upcoming orders")
+        for order in self.orders.items:
+            print("Customer:", order['customer'], "-- Flavor:",
+                  order['flavor'], "-- Scoops:", order['scoops'])
 
     def next_order(self):
-        # finish
+        remove_order = self.orders.dequeue()
+        print("\nNext Order Up!")
+        if remove_order is None:  # use none, not zero!
+            print("No upcoming orders")
+        else:
+            print("Customer:", remove_order['customer'], "-- Flavor:",
+                  remove_order['flavor'], "-- Scoops:", remove_order['scoops'])
+
+
+shop = IceCreamShop(["rocky road", "mint chip", "pistachio"])
+shop.take_order("Zachary", "pistachio", 3)
+shop.take_order("Marcy", "mint chip", 1)
+shop.take_order("Leopold", "vanilla", 2)
+shop.take_order("Bruce", "rocky road", 0)
+shop.show_all_orders()
+shop.next_order()
+shop.show_all_orders()
